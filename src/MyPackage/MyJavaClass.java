5d888445e9ae473c8edac475289e43641166992b
Hayriye Ozsoy <hayriye@Hayriyes-MacBook-Pro.local>
2019-12-26 13:20:58 -0600

    home work comporision operator

--- src/MyPackage/MyJavaClass.java
+++ src/MyPackage/MyJavaClass.java
@@ -1,43 +1,33 @@
 package MyPackage;
 
+
 public class MyJavaClass {
     public static void main(String[] args) {
 
-       //Question 1
-        String myStatement = "I am a good programmer";
-        System.out.println(myStatement.substring(11));
-        System.out.println("Total number of words : " +myStatement.codePointCount(0,17));
-        System.out.println(myStatement.replace('r','f'));
+        // Celsius to Kelvin
+       // T(K) = 20°C + 273.15 = 293.15 K
+        double celcius = 20;
+        double kelvin = (celcius+ 273.15);
+        System.out.println(kelvin);
 
-        //Question 2
-        String myName = "Hayriye Ozsoy";
-        System.out.println("Calculate My Name:" + myName.codePointCount(0,12));
+        // Celsius to Fahrenheit
+        // T(°F) = T(°C) × 9/5 + 32
+        double celcius2 = 45;
+        double fahrenheit =(celcius2) * 9/5 + 32;
+        System.out.println(fahrenheit);
 
-        //Question 3
-        String [] names = new String[8];
-        names [0] = "Mohammad";
-        names [1] = "jhon";
-        names [2] = "Ferdous";
-        names [3] = "Abdul";
-        names [4] = "Ratna";
-        names [5] = "Mareela";
-        names [6] = "Ruslan";
-        names [7] = "Himani";
-        System.out.println("JHON".contains(names[1]));
+        //Kelvin to Celcius
+        // T(°C) = 300K - 273.15 = 26.85 °C
+        double kelvin2 = 300;
+        double celcius3 =(kelvin2 - 273.15);
+        System.out.println(celcius3);
 
-        //Question 4
-        String [] friendNames = new String [5];
-        friendNames [0] = "Mirna";
-        friendNames [1] = "Canay";
-        friendNames [2] = "Erica";
-        friendNames [3] = "Daphne";
-        friendNames [4] = "Yeliz";
-
-        int size = friendNames.length;
-        System.out.println("The Array Length is: " + size);
-        System.out.println("Number 2 index name:" + friendNames[2]);
-        System.out.println("All names of friend: " + friendNames [0] + " " + friendNames[1] + " " + friendNames [2] + " " +friendNames[3] + " " + friendNames[4]);
-        System.out.println("2nd position name with A : " +friendNames [2].startsWith("A"));
+        //Kelvin to Fahrenheit
+         //T(°F) = T(K) × 9/5 - 459.67
+        double kelvin3 = 400;
+        double fahrenheit2 =(kelvin3) * 9/5 - 459.67;
+        System.out.println(fahrenheit2);
+        
 
 
 
@@ -45,6 +35,16 @@ public class MyJavaClass {
 
 
 
+
+
+
+
+
+        
+
+
+
+    }
 
 
 
@@ -55,5 +55,3 @@ public class MyJavaClass {
 
     }
 
-
-    }
