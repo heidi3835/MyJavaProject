808e050adc26ff91be57a075307b671ec2099c9e
Hayriye Ozsoy <hayriye@Hayriyes-MacBook-Pro.local>
2020-01-04 20:32:47 -0600

    Home Work about for loop

--- src/MyPackage/MyJavaClass.java
+++ src/MyPackage/MyJavaClass.java
@@ -1,117 +1,62 @@
 package MyPackage;
 
 public class MyJavaClass {
+
+
     public static void main(String[] args) {
-        //6. Create a supermarket item finder
-        //	Take input from user and based on the input, you should display user which aisle has the item
-        //    For example: user enter milk, you program should say aisle 8. If the item is not found,
-        //    display a message saying "Out of stock "Please create program for below requirement
-        //        Milk = Aisle Eight
-        //        Eggs = Aisle Eight
-        //        French Fries = Aisle seven
-        //        Candy = Aisle Three
-        //        Frozen Pizza = Aisle seven
-        //        Donuts = Aisle Eight
-        //        Diapers = Aisle One
-        //        Apple = Aisle two
-        //        Orange = Aisle two
 
-        int milk = '8';
-        switch (milk) {
-            case '1':
-                System.out.println("Aisle one Diapers should be aisle");
-                break;
+        ////Homework:
+        ////1.Students score, total possible score
+        //// 15/20 -> You got a C (75%)
+        //// A 90-100, B 80-89, C 70-79, D 60-69, F 0-59
+        int score = 55;
+        int maxPercent = 100;
+        int percent = (score * 100) / maxPercent;
 
-            case '2':
-                System.out.println("Aisle two Apple and oranges should be this aisle");
-                break;
-
-            case '3':
-                System.out.println("Aisle three Candy should be this aisle ");
-                break;
-
-            case '4':
-                System.out.println("Aisle four Kitchen stuff should be this aisle");
-
-                break;
-
-            case '5':
-                System.out.println("Aisle five Internetonial food should be this aisle");
-                break;
-
-            case '6':
-                System.out.println("Aisle seven Frozen food should be this aisle");
-                break;
-
-            case '7':
-                System.out.println("Aisle seven Soda should be this aisle");
-                break;
-
-            case '8':
-                System.out.println("Aisle eight milk out of stock");
-                break;
-
-            case '9':
-                System.out.println("Aisle nine ");
-                break;
-
-            default:
-                System.out.println("invalid number");
-
-                //questin 3
-
-
-
-
-        }
+        if (percent >= 90) {
+            System.out.println("Grade A");
+        } else if (percent > +80) {
+            System.out.println("Grade B");
+        } else if (percent >= 70) {
+            System.out.println("Grade C");
+        } else if (percent >= 60) {
+            System.out.println("Grade D");
+        } else if (percent >= 50) {
+            System.out.println("Grade E");
+        } else if (percent >= 40) {
+            System.out.println("Grade F");
         }
 
-    }
+        //2. Write a logic that prints multiplication table of the user provided number upto ten.
+
+          int x = 1;
+           for (int i= 1; i <= 10; i++) {
+               System.out.println(10 + " * " + i + " = " + 10 * i);
+
+           }
+
+           //3.Write a logic that checks if a number is a Prime number}
+            int num = 4;
+            int count = 0;
+
+            for (int i= 2; i <= num; i++) {
+                if (num % i == 0) {
+                    count = count + 1;
+                }
+                if (count > 0) {
+                    System.out.println("the number is not prime");
+                }
+                else {
+
+
+                    System.out.println("the number is prime:");
+                }
+
+
+            }
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }}
